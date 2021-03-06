20f0f7478c029831f4ca6ee5dc2567062513f87e
spaced-lord <zackarynoe@gmail.com>
2021-03-06 16:17:07 -0700

    change homepage url in package.json

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = () => {
   return (
-    <div className="container-xl ">
+    <div className="container-xl">
       <h1 className="h1">User Directory!</h1>
       <p className="lead">Sort users by name or search for a specific user."</p>
     </div>
