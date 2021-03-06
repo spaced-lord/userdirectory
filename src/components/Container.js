ccb207d909111ce85b485029a1dc71c7a4c772db
spaced-lord <zackarynoe@gmail.com>
2021-03-06 16:46:30 -0700

    Fixed wording in container

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,9 @@ const Container = () => {
   return (
     <div className="container-xl">
       <h1 className="h1">User Directory!</h1>
-      <p className="lead">Sort users by name or search for a specific user."</p>
+      <p className="lead">
+        Sort users by name or search for a specific user by first name."
+      </p>
     </div>
   );
 };
